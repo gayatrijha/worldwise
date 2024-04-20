57d6e387db5f4d46ab675945a424a4e8ff925cd2
Gayatri Jha <jha.gayatri01@gmail.com>
2024-04-20 14:06:41 +0530

    first commit

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageNav from "../components/PageNav";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <PageNav />
+      not found
+    </div>
+  );
+};
+
+export default PageNotFound;
